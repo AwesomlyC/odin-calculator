3173718f9a38b8916c900e7d1ddbf99e7e262141
AwesomlyC <109203183+AwesomlyC@users.noreply.github.com>
2024-03-03 17:28:11 -0800

    modified functionalities to be working properly

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ console.log("Working...");
 
 
 let num1;
-let operator;
+let operator = "";
 let num2;
 // Basic Functionalities
 function add(num1, num2){
@@ -30,55 +30,87 @@ function divide(num1, num2){
 }
 
 function operate(num1, operator, num2){
-
+    console.log("num1: " + num1 + " ... operator: " + operator + " ... num2: " + num2);
+    let result;
     switch (operator){
         case "+":
-            let sum = add(num1, num2);
-            console.log(sum);
+            result = add(num1, num2);
             break;
         case "-":
-            let difference = subtract(num1, num2);
-            console.log(difference);
+            result = subtract(num1, num2);
             break;
         case "*":
-            let total = multiply(num1, num2);
-            console.log(total);
+            result = multiply(num1, num2);
             break;
         case "/":
-            let divison = divide(num1, num2);
-            console.log(divison);
+            result = divide(num1, num2);
             break;
     }
+    return result;
+}
+
+function modifyText(value){
+    let display = document.querySelector(".text");
+    // let currentText = display.textContent;
+
+    if (value === "+" || value === "-" || value === "*" || value === "/"){
+        if (operator !== ""){
+            calculateResult();
+        }
+        operator = value;
+        console.log("operator is set to: " + operator);
+    }
+    display.textContent += value;
+    console.log(display.textContent);
+}
+
+function calculateResult(){
+    let display = document.querySelector(".text");
+    let text = display.textContent;
+    console.log("The operator to be used is ... " + operator)
+
+    let parsedText = text.split(operator);
+    result = operate(parseInt(parsedText[0]), operator, parseInt(parsedText[1]));
+
+    display.textContent = result;
+    operator="";
+
+}
+
+function clearResult(){
+    document.querySelector(".text").textContent = "";
+    operator = ""; 
 }
 
 
+
 // Testing...
-console.log("Testing sum...");
-operate(5,"+", 2)
-operate(0,"+", 2)
+// console.log("Testing sum...");
+// operate(5,"+", 2)
+// operate(0,"+", 2)
 
-operate(1,"+", 2)
+// operate(1,"+", 2)
 
-operate(1000,"+", 2)
+// operate(1000,"+", 2)
 
-console.log("Testing subtraction...");
-operate(5,"-", 2)
-operate(1,"-", 2)
-operate(0,"-", 2)
-operate(-4,"-", 2)
-operate(4,"-", -2)
+// console.log("Testing subtraction...");
+// operate(5,"-", 2)
+// operate(1,"-", 2)
+// operate(0,"-", 2)
+// operate(-4,"-", 2)
+// operate(4,"-", -2)
 
-console.log("Testing multiply...");
-operate(5, "*", 5);
-operate(0, "*", 5);
+// console.log("Testing multiply...");
+// operate(5, "*", 5);
+// operate(0, "*", 5);
 
-operate(-1, "*", 5);
+// operate(-1, "*", 5);
 
-operate(50000, "*", 5);
+// operate(50000, "*", 5);
 
 
-console.log("Testing divison...");
-operate(100,"/", 2)
-operate(23,"/", 5)
-operate(-9,"/", 2)
-operate(1000,"/", 0)
+// console.log("Testing divison...");
+// operate(100,"/", 2)
+// operate(23,"/", 5)
+// operate(-9,"/", 2)
+// operate(1000,"/", 0)
